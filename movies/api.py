e3277ef84cc09a075e56aa80397125d8e1c01599
HenryTan0705 <htan0705@gmail.com>
2020-09-16 19:28:52 -0400

    Movies-genre api

--- movies/api.py
+++ movies/api.py
@@ -1,12 +1,19 @@
 from rest_framework import viewsets, permissions
-from movies.models import Movie
-from .serializers import MovieSerializer
+from movies.models import Movie, Genre
+from .serializers import MovieSerializer, GenreSerializer
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
-
     permission_classes = [
         permissions.AllowAny
     ]
     serializer_class = MovieSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = GenreSerializer
--- movies/urls.py
+++ movies/urls.py
@@ -11,9 +11,10 @@
 
 
 from rest_framework import routers
-from .api import MovieViewSet
+from .api import MovieViewSet, GenreViewSet
 
 router = routers.DefaultRouter()
 router.register('api/movies', MovieViewSet, 'movies')
+router.register('api/genres', GenreViewSet, 'genres')
 
 urlpatterns = router.urls
